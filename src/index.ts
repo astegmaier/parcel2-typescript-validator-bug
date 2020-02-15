20138a70ea24c322c304071c81efec203c1262d1
Andrew Stegmaier <ansteg@microsoft.com>
2020-02-14 16:55:47 -0800

    Add transformer-typescript-tsc.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-const message: number = "Hello world!";
 
-console.log(message);
+// This should only compile with tsc (it requires special plugins for babel).
+const enum Direction {
+    Left,
+    Right,
+    Top,
+    Down
+}
+
+const message: number = "Hello world!"; // This is a type error!
+console.log(Direction.Left);
