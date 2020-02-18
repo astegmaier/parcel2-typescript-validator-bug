37d8be280f6042aec96221a213de881efbd64a13
Andrew Stegmaier <ansteg@microsoft.com>
2020-02-18 15:37:30 -0800

    Update parcel debug config.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 const message: number = "Hello World"; // This is a type error!
 
-const App = () => <div>{message}</div>
+const App = () => <div>{message}</div>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
