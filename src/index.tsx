7d6fecb996482a142f9e38cee0f875b68bc9469d
Andrew Stegmaier <ansteg@microsoft.com>
2020-02-19 11:45:22 -0800

    Add back type error.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const message: string = "A new Message!"; // This is a type error!
+const message: number = "A new Message!"; // This is a type error!
 
 ReactDOM.render(<App message={message} />, document.getElementById("root"));
