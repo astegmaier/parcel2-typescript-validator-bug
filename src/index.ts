e6bbc97ac3fc971d850bb2d9ff14cbc218ea40f8
Andrew Stegmaier <ansteg@microsoft.com>
2020-03-19 10:36:37 -0400

    Create a .ts file that depends on another one.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-const message: string = "Hello World!";
-console.log(message);
-
-// @ts-ignore
-process.browser = false;
-// @ts-ignore
-module.exports = function() {
-  // @ts-ignore
-  return (process as any).browser && test((process as any).browser);
-};
-
-function test(val: any) {
-  return val;
-}
+import { returnMessage } from "./returnMessage";
+const message: string = "My Message!";
+export const output = returnMessage(message);
---
+++ src/returnMessage.ts
@@ -0,0 +1,3 @@
+export function returnMessage(message: string): string {
+  return message;
+}
